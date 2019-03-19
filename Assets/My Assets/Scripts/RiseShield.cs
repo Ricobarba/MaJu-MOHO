f11a2d2f62a9b0da13ec427a1a920c191d0c85c6
Tran Hoai Tri Thien Martin <martin.tranhoai@yahoo.fr>
2019-03-19 23:50:44 +0100

    Martin 19/03_23h50

---
+++ Assets/My Assets/Scripts/RiseShield.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiseShield : MonoBehaviour
+{
+    public Character1Controller charac;
+    public GameObject shield;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_gameObjectToActivate.SetActive(_setActive);
+        if(Input.GetButtonDown(charac.guardStr))
+        {
+            shield.SetActive(true);
+        }
+
+        if(Input.GetButtonUp(charac.guardStr))
+        {
+            shield.SetActive(false);
+        }
+
+    }
+}
